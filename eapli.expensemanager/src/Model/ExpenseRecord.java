02f3e4111f9f8c37968b8635db967e488330cfd4
1071223 Luís Conceição <1071223@isep.ipp.pt>
2013-04-05 15:37:47 +0100

    Criação de ExpenseRecord 1071223

---
+++ eapli.expensemanager/src/Model/ExpenseRecord.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ *
+ * @author lmsc
+ */
+public class ExpenseRecord {
+    private List<Expense> list;
+    
+    public ExpenseRecord(List<Expense> list){
+        this.list=list;
+    }
+    
+    public BigDecimal getTotal(){
+        BigDecimal total=new BigDecimal(0);
+        for(Expense exp:list){
+            total=total.add(exp.getAmount());
+        }
+        return total;        
+    }
+}
