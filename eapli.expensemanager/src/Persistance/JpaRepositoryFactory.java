9e6899b47a54397beaeac0e6dc5b29ed4a3e3e30
ruidamiao1100669 <1100669@isep.ipp.pt>
2013-05-16 17:46:59 +0100

    alteracao persistence  1100669 1071223 1111112

--- eapli.expensemanager/src/Persistance/JpaRepositoryFactory.java
+++ eapli.expensemanager/src/Persistance/JpaRepositoryFactory.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Persistance.JPA;
+package Persistance;
 
 import Persistance.IExpenseRepository;
 import Persistance.IExpenseTypeRepository;
