6c5144b612d32b1e1bea1a00a15b06e630dfd598
LCAlex <1120066@isep.ipp.pt>
2013-05-17 15:56:51 +0100

    Classe PaymentMeansJPA acabada
    1120066
    LCAlex

---
+++ eapli.expensemanager/src/Persistance/JPA/PaymentMeansJPA.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistance.JPA;
+
+/**
+ *
+ * @author mcatia
+ */
+import Model.PaymentMean;
+import java.util.List;
+import Persistance.IPaymentMeansRepository;
+import eapli.util.DateTime;
+import java.util.Calendar;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Rui Damiao
+ */
+public class PaymentMeansJPA extends JpaGeneric<PaymentMean, Long> implements IPaymentMeansRepository {
+
+    @Override
+    public void registerPaymentMean(PaymentMean pm) {
+        save(pm);
+    }
+
+    @Override
+    public void showListPayMeans() {
+        List<PaymentMean> listPayments = all();
+        for (int i = 0; i < listPayments.size(); i++) {
+            System.out.println("- " + listPayments.get(i).getClass().getName());
+            System.out.println(listPayments.get(i));
+        }
+        System.out.println("\n");
+        //TODO testes unitarios
+    }
+
+    @Override
+    public List<PaymentMean> getListPayMeans() {
+        return all(); //To change body of generated methods, choose Tools | Templates.
+    }
+}
