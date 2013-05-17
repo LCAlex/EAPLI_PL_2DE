4d50dcc82e3e6e0888e2da4112d55d501ffda3af
ruidamiao1100669 <1100669@isep.ipp.pt>
2013-05-17 15:27:31 +0100

    Persistencia das despesas e tipo de despesas   1100669 1071223 1111112 fixes #36

---
+++ eapli.expensemanager/src/Persistance/JPA/ExpenseTypeJpa.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistance.JPA;
+
+
+import Model.ExpenseType;
+import java.util.List;
+import Persistance.IExpenseTypeRepository;
+
+/**
+ *
+ * @author Rui Damiao
+ */
+public class ExpenseTypeJpa extends JpaGeneric<ExpenseType, Long> implements IExpenseTypeRepository{
+      
+    @Override
+      public void saveExpenseType(ExpenseType eT){
+            save(eT);
+      }
+    
+    @Override
+     public List<ExpenseType> getAllExpenseTypes(){
+            return all();
+      }
+}
